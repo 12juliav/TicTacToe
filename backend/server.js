fa194cb2b66ba859bdc1b3b5bc8f83aa625eb3fa
Julia <12juliav@gmail.com>
2022-08-30 22:43:06 -0700

    a

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use('/', routes);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+	app.use(express.static(path.join('/', 'frontend', 'build')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+		res.sendFile(path.join('/', 'frontend', 'build', 'index.html'))
 	});
 
 	app.use((req, res, next) => {
