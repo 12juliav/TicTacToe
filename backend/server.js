749fc8c381a7ebced46e88596af78b8e1d4a4518
Julia <12juliav@gmail.com>
2022-08-30 22:14:44 -0700

    test

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,25 @@ app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
-if (process.env.NODE_ENV == 'production') 
-{
-  app.use(express.static('frontend/build'));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-  app.get('*', (req, res) => 
- {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+	});
+
+	app.use((req, res, next) => {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader(
+			'Access-Control-Allow-Headers',
+			'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+		);
+		res.setHeader(
+			'Access-Control-Allow-Methods',
+			'GET, POST, PATCH, DELETE, OPTIONS'
+		);
+		next();
+	});
 }
  
 app.listen(port, () => {
