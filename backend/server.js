28b116235ec1c32bcb8f711f7ee188d268935349
Julia <12juliav@gmail.com>
2022-08-30 22:37:05 -0700

    a

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const path = require('path');
 const port = process.env.PORT || 5000;
 const routes = require('./routes/index');
 
